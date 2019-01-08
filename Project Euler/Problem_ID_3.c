74692ba8b9926123ed03f4b5cc866b0d4ca6d334
Anim <Anim-101@users.noreply.github.com>
2019-01-08 22:43:46 +0600

    Add files via upload

---
+++ Project Euler/Problem_ID_3.c
@@ -0,0 +1,37 @@
+/*
+Problem ID 3: Largest Prime Number
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    long long int number = 600851475143;
+    long long int factor = 2;
+
+    while(1)
+    {
+        if( number == factor)
+        {
+            break;
+        }
+        else
+        {
+
+            if(number % factor == 0)
+            {
+                number = number / factor;
+                factor = 2;
+            }
+            else
+            {
+                factor++;
+
+            }
+        }
+    }
+
+    printf("Largest Prime Factor: %lld", factor);
+
+    return 0;
+}
