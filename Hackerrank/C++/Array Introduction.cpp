2c2e728cd83a0212225a07922d8a0c2ece5da6ad
Anim <Anim-101@users.noreply.github.com>
2019-07-11 18:48:53 +0600

    Add files via upload

---
+++ Hackerrank/C++/Array Introduction.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+
+    scanf("%d", &num);
+
+    int arr[num];
+
+    for(int i = 0; i < num; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = num - 1; i >= 0; i--)
+    {
+        if(i != 0)
+        {
+            printf("%d ", arr[i]);
+        }
+        else if(i == 0)
+        {
+            printf("%d", arr[i]);
+        }
+    }
+}
