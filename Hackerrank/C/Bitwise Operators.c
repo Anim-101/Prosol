69d0abbbfa9cfd3fa06da366419f6c18e89988ba
Anim <Anim-101@users.noreply.github.com>
2019-12-06 00:57:03 +0600

    Add files via upload

---
+++ Hackerrank/C/Bitwise Operators.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+
+void calculate_the_maximum(int n, int k)
+{
+    int maxAnd = 0, maxOr = 0, maxXor = 0;
+
+    for(int i = 1; i < n; i++)
+    {
+       for(int j = i + 1; j <= n; j++)
+       {
+            if(((i & j) < k) && ((i & j) > maxAnd))
+            {
+                maxAnd = i & j;
+            }
+
+            if(((i | j) < k) && ((i | j) > maxOr))
+            {
+                maxOr = i | j;
+            }
+
+            if(((i ^ j) < k) && ((i ^ j) > maxXor))
+            {
+                maxXor = i ^ j;
+            }
+        }
+    }
+
+    printf("%d\n%d\n%d", maxAnd, maxOr, maxXor);
+}
+
+int main()
+{
+    int n, k;
+
+    scanf("%d %d", &n, &k);
+
+    calculate_the_maximum(n, k);
+
+    return 0;
+}
